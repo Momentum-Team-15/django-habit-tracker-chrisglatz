2d990e7f62614a6677bce9d87ee97c8313587831
boxdaddy <cg.glatz@gmail.com>
2022-11-06 10:23:40 -0500

    changed path of manage.py in build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pipenv install
 
-pipenv run python manage.py migrate
+pipenv run python /habittracker/manage.py migrate
 pipenv run python manage.py collectstatic --no-input
 pipenv run python manage.py createsuperuser --no-input
